42decbcb9112ece141c9bcc4f74f10a4be7b41c9
Meiyalagan Ramadurai <meiyalagan.ramadurai@in.abb.com>
2024-09-04 15:11:59 +0530

    Initial Commit

---
+++ SampleWatchApp Watch App/SampleWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWatchAppApp.swift
+//  SampleWatchApp Watch App
+//
+//  Created by Karthik.Kurdekar on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
